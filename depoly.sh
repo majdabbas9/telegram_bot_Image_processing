c252f3f73c25c668e16ebf42a9de246c653fdf34
majdabbas <majd.abbas999@gmail.com>
2025-05-13 23:18:04 +0300

    Version 1.9 - adding automation deploy

---
+++ depoly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# copy the .servcie file
+sudo cp polybot.service /etc/systemd/system/
+
+# reload daemon and restart the service
+sudo systemctl daemon-reload
+sudo systemctl restart polybot.service
+sudo systemctl enable polybot.service
+
+# Check if the service is active
+if ! systemctl is-active --quiet polybot.service; then
+  echo "❌ polybot.service is not running."
+  sudo systemctl status polybot.service --no-pager
+  exit 1
+fi
