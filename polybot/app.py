418e111a7c0b7baaf37633d1668c9379627291a8
majdabbas <majd.abbas999@gmail.com>
2025-05-31 01:08:00 +0300

    Version 1.10.5 adding and testing Nginx

--- polybot/app.py
+++ polybot/app.py
@@ -25,5 +25,5 @@ def webhook():
 
 
 if __name__ == "__main__":
-    bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, "https://34.241.159.6")
+    bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, "https://majd_app.fursa.click")
     app.run(host='0.0.0.0', port=8443)
