3f91f72856f8a599f24714cd29b85451dca8cae2
majdabbas <majd.abbas999@gmail.com>
2025-05-31 17:40:42 +0300

    Version 1.10.5 adding and testing Nginx

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -113,7 +113,7 @@ if ! systemctl is-active --quiet polybot_dev.service; then
   sudo systemctl status polybot_dev.service --no-pager
   exit 1
 fi
-echo "$app_dev_cert" > polybot_cer.crt
+printf "%b" "$app_dev_cert" > polybot_cer.crt
 env_file="$path_to_file/polybot/.env"
 echo "TELEGRAM_BOT_TOKEN=$telegram_token" > "$env_file"
 echo "S3_BUCKET_NAME=$s3_bucket_name_dev" >> "$env_file"
