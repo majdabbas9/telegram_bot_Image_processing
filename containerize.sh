8d634be9cc6eedc5af235952847ffb93fc5d0bf1
majdabbas <majd.abbas999@gmail.com>
2025-06-11 23:49:37 +0300

    Version 1.10.6 fixing test problem

---
+++ containerize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script deletes all Docker images with the name 'majdabbas99/polybot_build_dev'
+image_name=$1
+prefix="${image_name%%:*}:"
+
+sudo docker images --format "{{.Repository}}:{{.Tag}} {{.ID}}" | \
+grep "^$prefix" | \
+grep -v "$image_name" | \
+awk '{print $2}' | xargs -r sudo docker rmi
+
+
+sudo docker compose -f docker-compose.polybot.yaml down
+sudo docker compose -f docker-compose.polybot.yaml up -d
