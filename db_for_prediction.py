bf7d57dd0c24eb48f3aaaa3c288bfe6ef1daefcd
majdabbas <majd.abbas999@gmail.com>
2025-06-17 03:26:41 +0300

    Version 1.10.8 fixing bugs

---
+++ db_for_prediction.py
@@ -0,0 +1,28 @@
+import boto3
+from datetime import datetime
+from decimal import Decimal
+from botocore.exceptions import ClientError
+
+
+class DynamoDBDatabaseHandler:
+    def __init__(self, env='dev', table_prefix='majd_yolo'):
+        self.dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
+        self.prefix = f"{env}_{table_prefix}"  # e.g., "dev_majd_yolo"
+
+        self.prediction_sessions_table = self.dynamodb.Table(f"{self.prefix}_prediction_sessions")
+        self.detection_objects_table = self.dynamodb.Table(f"{self.prefix}_detection_objects")
+
+        self.init_db()
+
+    def init_db(self):
+        try:
+            self.prediction_sessions_table.load()
+            self.detection_objects_table.load()
+        except ClientError as e:
+            raise RuntimeError("One or more DynamoDB tables do not exist") from e
+
+
+    def get_predicted_image(self, uid):
+        response = self.prediction_sessions_table.get_item(Key={'uid': uid})
+        item = response.get('Item')
+        return item.get('predicted_image') if item else None
--- polybot/app.py
+++ polybot/app.py
@@ -3,10 +3,10 @@ from flask import request
 import os
 from polybot.bot import Bot, QuoteBot, ImageProcessingBot
 app = flask.Flask(__name__)
-
+from dotenv import load_dotenv
+load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 Nginx_url = os.getenv('NGINX_URL')
-
 @app.route('/', methods=['GET'])
 def index():
     return 'Ok'
@@ -19,6 +19,18 @@ def webhook():
     return 'Ok'
 
 
+
+@app.route(f'/predictions/', methods=['POST'])
+def send_detected_objects():
+    req = request.get_json()
+    print(req)
+    uid = req.get('uid')
+    chat_id = req.get('chat_id')
+    file_path = req.get('file_path')
+    image_url = req.get('image_url')
+    bot.handle_callback_yolo(uid, chat_id, file_path,image_url)
+    return 'Ok'
+
 if __name__ == "__main__":
-    bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, Nginx_url)
+    bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, Nginx_url,test_mode=True)
     app.run(host='0.0.0.0', port=8443)
