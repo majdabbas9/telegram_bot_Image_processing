6b4417c9a22738b0b6a5a1bcfa4b02df7d80d496
majdabbas <majd.abbas999@gmail.com>
2025-06-17 03:32:36 +0300

    Version 1.10.8 adding dynamodb

--- polybot/app.py
+++ polybot/app.py
@@ -5,7 +5,6 @@ from polybot.bot import Bot, QuoteBot, ImageProcessingBot
 app = flask.Flask(__name__)
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 Nginx_url = os.getenv('NGINX_URL')
-
 @app.route('/', methods=['GET'])
 def index():
     return 'Ok'
@@ -17,6 +16,18 @@ def webhook():
     bot.handle_message(req['message'])
     return 'Ok'
 
+
+
+@app.route(f'/predictions/', methods=['POST'])
+def send_detected_objects():
+    req = request.get_json()
+    print(req)
+    uid = req.get('uid')
+    chat_id = req.get('chat_id')
+    file_path = req.get('file_path')
+    image_url = req.get('image_url')
+    bot.handle_callback_yolo(uid, chat_id, file_path,image_url)
+    return 'Ok'
 
 if __name__ == "__main__":
     bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, Nginx_url)
